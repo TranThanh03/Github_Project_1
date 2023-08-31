773d709db4a632d3fc18b535d32d0708b2560877
TranThanh03 <thanhhkh3@gmail.com>
2023-08-31 23:28:17 +0700

    Add files

--- script.js
+++ script.js
@@ -1,39 +1,31 @@
-
 document.addEventListener("DOMContentLoaded", function () {
     const loginForm = document.querySelector(".login-form");
     const usernameInput = document.querySelector(".username");
     const passwordInput = document.querySelector(".password");
     const errorMessage = document.getElementById("error");
-    
+    const toggleButton = document.getElementById("toggle-button");
+
     loginForm.addEventListener("submit", function (e) {
         e.preventDefault();
 
         const dataUsername = usernameInput.value;
         const dataPassword = passwordInput.value;
-        if (dataUsername === "") {
+        if(dataUsername === "") {
             usernameInput.style.borderBottom = "2px solid red";
-            return false;
         }
-        else {
+        else{
             usernameInput.style.borderBottom = "2px solid black";
-            if (dataUsername === 'thanh2k3' && dataPassword === '22102003') {
+            if(dataUsername === 'thanh2k3' && dataPassword === '22102003') {
                 loginForm.submit();
-                return true;
             }
-            else {
+            else{
                 errorMessage.style.display = "block";
                 setTimeout(function() {
                     errorMessage.style.display = "none";
-                }, 1300);
-                return false;
+                }, 1200);
             }
         }
     });
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-    const passwordInput = document.querySelector(".password");
-    const toggleButton = document.getElementById("toggle-button");
     
     toggleButton.addEventListener("click", function() {
         if(passwordInput.type === "password") {
@@ -41,11 +33,10 @@ document.addEventListener("DOMContentLoaded", function() {
             toggleButton.querySelector("i").classList.remove("fa-solid", "fa-lock");
             toggleButton.querySelector("i").classList.add("fa-solid", "fa-lock-open");
         }
-        else {
+        else{
             passwordInput.type = "password";
             toggleButton.querySelector("i").classList.remove("fa-solid", "fa-lock-open");
             toggleButton.querySelector("i").classList.add("fa-solid", "fa-lock");
         }
     });
 });
-
