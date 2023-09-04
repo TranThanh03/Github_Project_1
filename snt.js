dc3edd3114dcd504417bea887ba8b741c223fde9
TranThanh03 <107708016+TranThanh03@users.noreply.github.com>
2023-09-04 16:15:08 +0700

    Update snt.js

--- snt.js
+++ snt.js
@@ -1,31 +1,43 @@
-function kt(a) {
-    if(a<2)
+function checkSnt(i) {
+    if(i<2)
         return false;
     else{
-        for(let i=2; i<=Math.sqrt(a); i++){
-            if(a % i === 0)
+        for(let j=2; j<=Math.sqrt(i); j++){
+            if(i % j === 0)
                 return false;
         }
         return true;
     }
 }
 
-
-function check() {
-    let arr = [];
+function checkInput() {
+    let array = [];
     const n = parseInt(document.getElementById("input").value);
+    const outputData = document.getElementById("output");
     for (let i = 1; i <= n; i++) {
-        if (kt(i) === true){
-            arr.push(i);
+        if (checkSnt(i) === true){
+            array.push(i);
         }
     }
-    document.getElementById("output").value = arr.join(", ");
-    const array = 'Thanh Tran Van 2210'; 
+    outputData.value = array.join(", ");
     const outputWidth = array.length * 100;
-    // outputWidth = Math.min(Math.max(outputWidth, 50), 300);
-    output.style.width = outputWidth + 'px';
+    outputData.style.width = Math.min(Math.max(outputWidth, 50), 300) + 'px';
 }
 
-function xoa() {
-    output.style.width = "";
+function resetData() {
+    const outputData = document.getElementById("output");
+    outputData.style.width = "";
 }
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const checkFont = document.querySelector(".login-form");
+
+    checkFont.addEventListener("submit", function(e) {
+        e.preventDefault();
+        const resetButton = document.getElementById("reset");
+        
+        resetButton.addEventListener("click", resetData);
+        checkInput();
+    });
+});
